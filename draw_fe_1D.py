a2c7cc21703ec4593cb929f238933b79f421e426
hanatok <summersnow9403@gmail.com>
2017-09-19 18:23:03 +0800

    Add colvars units support in 1D script.

--- draw_fe_1D.py
+++ draw_fe_1D.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser()
 parser.add_argument("pmf", help = "specify the PMF file")
 parser.add_argument("-o", "--output", help = "specify the PNG output image file")
+parser.add_argument("-c", "--colvars", action = "store_true", help = "use Colvars units")
 args = parser.parse_args()
 pmf = args.pmf
 if args.output is None:
@@ -17,7 +18,8 @@ else:
 
 x, y = np.genfromtxt(pmf, unpack=True)
 y = y + abs(min(y))
-y = y/4.184
+if args.colvars == False:
+    y = y / 4.184
 plt.plot(x, y)
 plt.xlabel("Distance(nm)")
 plt.ylabel("Free energy(kCal/mol)")
