0cf84870f981c76cc11e77ddf34073e102c0fef2
hanatok <summersnow9403@gmail.com>
2017-09-20 10:38:57 +0800

    Add option to shift PMF with min-to-zero

--- draw_fe_1D.py
+++ draw_fe_1D.py
@@ -2,6 +2,7 @@
 import argparse
 import numpy as np
 import matplotlib
+import os
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
 
@@ -9,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("pmf", help = "specify the PMF file")
 parser.add_argument("-o", "--output", help = "specify the PNG output image file")
 parser.add_argument("-c", "--colvars", action = "store_true", help = "use Colvars units")
+parser.add_argument("-s", "--save", action = "store_true", help = "save new PMF file with min-to-zero")
 args = parser.parse_args()
 pmf = args.pmf
 if args.output is None:
@@ -26,3 +28,8 @@ plt.ylabel("Free energy(kCal/mol)")
 ax = plt.gca()
 ax.tick_params(direction="in")
 plt.savefig(png, dpi=400, transparent=True)
+
+if args.save is True:
+    fn = os.path.basename(pmf)
+    newpmf = "new_" + fn
+    np.savetxt(newpmf, np.transpose([x, y]), delimiter = "\t", fmt = "%10.7f")
