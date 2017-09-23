005b0ba43e416942a26fc4d3766139ddf2a15b6e
hanatok <summersnow9403@gmail.com>
2017-09-23 18:14:28 +0800

    Add script for plotting RMSD

---
+++ plot_rmsd.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import argparse
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('rmsd', nargs = '+', help = 'specify the RMSD data file(s)')
+parser.add_argument('-o', '--output', help = 'PNG output file') 
+args = parser.parse_args()
+data = args.rmsd
+outpng = args.output
+
+plt.figure()
+y = np.genfromtxt(data[0], unpack=True, usecols=1)
+x = np.linspace(1, len(y), len(y))
+for file in data:
+    lb = os.path.splitext(file)[0]
+    y = np.genfromtxt(file, unpack=True, usecols=1)
+    plt.plot(x,y,label=lb)
+plt.xlabel('Simulation time(ns)')
+plt.ylabel('Free energy RMSD(kcal/mol)')
+plt.tick_params(direction='in')
+leg = plt.legend()
+leg.get_frame().set_alpha(0.0)
+plt.savefig(outpng, dpi=400, transparent=True)
