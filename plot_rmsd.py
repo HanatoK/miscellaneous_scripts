f814c883ca5c151b597e49a133a31f57a5ac6991
hanatok <summersnow9403@gmail.com>
2017-10-07 09:34:23 +0800

    Add new option to specify lines in plot_rmsd.py

--- plot_rmsd.py
+++ plot_rmsd.py
@@ -8,17 +8,24 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('rmsd', nargs = '+', help = 'specify the RMSD data file(s)')
-parser.add_argument('-o', '--output', help = 'PNG output file') 
+parser.add_argument('-o', '--output', help = 'PNG output file')
+parser.add_argument('-l', '--line', type = int, help = 'PNG output file')
 args = parser.parse_args()
 data = args.rmsd
 outpng = args.output
 
+
 plt.figure()
 y = np.genfromtxt(data[0], unpack=True, usecols=1)
-x = np.linspace(1, len(y), len(y))
+if args.line is None:
+    nl = len(y)
+else:
+    nl = args.line
+x = np.linspace(1, nl, nl)
 for file in data:
     lb = os.path.splitext(file)[0]
     y = np.genfromtxt(file, unpack=True, usecols=1)
+    y = y[0:nl]
     plt.plot(x,y,label=lb)
 plt.xlabel('Simulation time(ns)')
 plt.ylabel('Free energy RMSD(kcal/mol)')
