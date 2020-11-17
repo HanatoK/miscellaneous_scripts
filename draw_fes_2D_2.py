7b07ca8f96c56fba3a7c3fedee81416baf66c0ff
hanatok <summersnow9403@gmail.com>
2020-11-16 19:37:27 -0600

    Update draw_fes_2D_2.py

--- draw_fes_2D_2.py
+++ draw_fes_2D_2.py
@@ -9,16 +9,16 @@ plt.rcParams.update({
     "font.family": "serif",  # use serif/main font for text elements
     "text.usetex": True,     # use inline math for ticks
     "pgf.rcfonts": False,    # don't setup fonts from rc parameters
-    "axes.labelsize": 20,
+    "axes.labelsize": 28,
     "axes.linewidth": 2.0,
-    "font.size": 16,
-    "pgf.preamble": [
+    "font.size": 24,
+    "pgf.preamble": '\n'.join([
          "\\usepackage{units}",          # load additional packages
          "\\usepackage{metalogo}",
          "\\usepackage{unicode-math}",   # unicode math setup
          r"\setmathfont{MathJax_Math}",
          r"\setmainfont{Arimo}",  # serif font via preamble
-         ]
+         ])
 })
 import numpy as np
 from matplotlib.figure import figaspect
@@ -34,7 +34,7 @@ parser.add_argument("--ytitle", default = "CV2", help = "title along Y axis")
 parser.add_argument("--levels", default = 25, type = int, help = "number of levels")
 args = parser.parse_args()
 
-def plotfes(pmffilename, pngfilename, xtitle, ytitle):
+def plotfes(pmffilename, pngfilename, xtitle, ytitle, level):
     x, y, z = np.genfromtxt(pmffilename, unpack=True)
     binx = len(set(x))
     biny = len(set(y))
@@ -42,29 +42,30 @@ def plotfes(pmffilename, pngfilename, xtitle, ytitle):
     yi = y.reshape(binx, biny)
     zi = z.reshape(binx, biny)
     fig = plt.figure()
-    cf = plt.contourf(xi, yi, zi, args.levels, cmap=plt.cm.jet)
+    cf = plt.contourf(xi, yi, zi, level, cmap=plt.cm.jet)
     plt.xlabel(xtitle)
     plt.ylabel(ytitle)
-    plt.title('Free Energy Surface')
+    #plt.title('Free Energy Surface')
     ax = plt.gca()
     ax.tick_params(direction = 'in', which = 'major', length=6.0, width = 1.0, top = True, right = True)
     ax.tick_params(direction = 'in', which = 'minor', length=3.0, width = 1.0, top = True, right = True)
     ax.xaxis.get_major_formatter()._usetex = False
     ax.yaxis.get_major_formatter()._usetex = False
-    #ax.xaxis.set_major_locator(MultipleLocator(0.01))
-    #ax.yaxis.set_major_locator(MultipleLocator(0.05))
     ax.xaxis.set_minor_locator(AutoMinorLocator())
     ax.yaxis.set_minor_locator(AutoMinorLocator())
+    ax.xaxis.set_major_locator(plt.MaxNLocator(6))
+    ax.yaxis.set_major_locator(plt.MaxNLocator(6, prune='lower'))
     clb = plt.colorbar()
-    clb.ax.set_title("kcal/mol")
+    clb.ax.set_title("kcal/mol", fontsize=20)
     clb.ax.xaxis.get_major_formatter()._usetex = False
     clb.ax.yaxis.get_major_formatter()._usetex = False
-    clbticks = list(cf.levels)
+    clbticks = [float(i.get_position()[1]) for i in clb.ax.get_yticklabels()]
     clbticksstr = ['{:.1f}'.format(i) for i in clbticks]
-    clb.ax.set_title("Error (kcal/mol)", fontsize = 16, pad = 10.0)
-    clb.ax.set_yticklabels(clbticksstr, fontsize = 16)
-    plt.tight_layout(pad = 0.1)
-    plt.savefig(pngfilename, dpi=400, transparent=True)
+    print(clbticksstr)
+    clb.ax.set_yticklabels(clbticksstr, fontsize = 20)
+    plt.savefig(pngfilename, dpi=400, bbox_inches = 'tight', transparent=True)
     return
 
-plotfes(args.pmf, args.output, xtitle = args.xtitle, ytitle = args.ytitle)
+plotfes(args.pmf, args.output, xtitle = args.xtitle, ytitle = args.ytitle, args.level)
+#plotfes('trialanine_apath_cpp+1.czar.pmf', 'apath_2d.png', r'$\xi (s)$', r'$\xi (z)$')
+#plotfes('trialanine_gpath_2d.czar.pmf', 'gpath_2d.png', r'$\xi (s)$', r'$\xi (z)$')
