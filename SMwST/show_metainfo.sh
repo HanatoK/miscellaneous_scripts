e8ad57bdc248e7cc23feb6ff5f2ec8b9ac4d10d8
me <summersnow9403@gmail.com>
2021-08-27 20:39:25 -0500

    Fix show_metainfo.sh

--- SMwST/show_metainfo.sh
+++ SMwST/show_metainfo.sh
@@ -8,7 +8,7 @@ num_swarm_steps=$(grep 'num_swarm_steps' swarms.conf|awk '{print $3}')
 num_equil_steps=$(grep 'num_equil_steps' swarms.conf|awk '{print $3}')
 num_total_replicas=$(grep -oP '(?<=replicas )[0-9]+' run.sh)
 time_swarm=$(bc <<< "$num_swarm_steps * 0.5")
-time_equil=$(bc <<< "$num_equil_steps * 0.5")
+time_equil=$(bc <<< "$num_equil_steps * 0.5 / 1000")
 num_copies=$(bc <<< "$num_total_replicas / $num_images")
 echo "============================================================================"
 echo "Simulation $1"
@@ -16,6 +16,6 @@ echo "==========================================================================
 printf "Number of images: %d\n" $num_images
 printf "Number of copies: %d\n" $num_copies
 printf "Number of iterations: %d\n" $num_iterations
-printf "Simulation time of restrained equilibration: %10.2f fs\n"  $time_swarm
-printf "Simulation time of forming swarms of trajectories: %10.2f fs\n"  $time_equil
+printf "Simulation time of restrained equilibration: %10.2f ps\n"  $time_equil
+printf "Simulation time of forming swarms of trajectories: %10.2f fs\n"  $time_swarm
 echo "============================================================================"
