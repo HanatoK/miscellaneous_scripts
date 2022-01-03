0b37855c4af9e097901e79d7602107c3c97c9d40
HanatoK <summersnow9403@gmail.com>
2022-01-03 09:34:23 -0600

    Update collect-configuration.py and draw_axis.tcl
    1. Collect all copies in collect-configuration.py;
    2. Draw tick marks in draw_axis.tcl.

--- SMwST/collect-configuration.py
+++ SMwST/collect-configuration.py
@@ -11,7 +11,8 @@ args = parser.parse_args()
 num_images = int(args.n)
 
 # get the number of trajectories per swarm
-output_dirs = glob('output/*')
+output_basedir = 'output'
+output_dirs = glob(f'{output_basedir}/*')
 num_swarms = int(len(output_dirs) / num_images)
 print(f'Number of images : {num_images}')
 print(f'Number of trajectories per image : {num_swarms}')
@@ -20,39 +21,46 @@ result_dirs = output_dirs[::num_swarms]
 
 # get the number of iterations
 num_iterations = len(glob(result_dirs[0] + '/*.image*.iter*.coor'))
-digits_iterations = len(str(num_iterations))
+digits_iterations = len(str(num_iterations-1))
 print(f'Number of iterations: {num_iterations}')
 
 # make directories and copy images
 base_directory = os.getcwd() + '/string_results'
 if not os.path.exists(base_directory):
     os.makedirs(base_directory)
-i_image = 0
-i_iteration = 0
 digits_images = len(str(num_images))
-for result_dirname in result_dirs:
-    image_dirname = base_directory + '/' + str(i_image).zfill(digits_images)
+digits_copies = len(str(num_swarms))
+digits_output = len(str(num_swarms*num_images))
+for i_image in range(0, num_images):
+    image_dirname = base_directory + '/image_' + str(i_image).zfill(digits_images)
     if not os.path.exists(image_dirname):
         os.makedirs(image_dirname)
-    iteration_images = glob(result_dirname + '/*.image*.iter*.coor')
-    iteration_images.sort()
-    for image_name in iteration_images:
-        dest_name = image_dirname + '/' + str(i_iteration).zfill(digits_iterations) + '.coor'
-        print(f'Copying file from {image_name} to {dest_name}')
-        copyfile(image_name, dest_name)
-        i_iteration = i_iteration + 1
-    i_image = i_image + 1
-    i_iteration = 0
+    for j_copy in range(0, num_swarms):
+        copy_dirname = image_dirname + '/copy_' + str(j_copy).zfill(digits_copies)
+        if not os.path.exists(copy_dirname):
+            os.makedirs(copy_dirname)
+        result_dirname = output_basedir + '/' + str(i_image * num_swarms + j_copy).zfill(digits_output)
+        iteration_images = glob(result_dirname + '/*.image*.iter*.coor')
+        iteration_images.sort()
+        for i_iteration, image_name in enumerate(iteration_images):
+            dest_name = copy_dirname + '/' + str(i_iteration).zfill(digits_iterations) + '.coor'
+            print(f'Copying file from {image_name} to {dest_name}')
+            copyfile(image_name, dest_name)
 
 # collect the final path into a separate directory
 print('Collect the images of the final path:')
 final_path_directory = os.getcwd() + '/final_path'
 if not os.path.exists(final_path_directory):
     os.makedirs(final_path_directory)
-for i_image in range(0, num_images):
-    image_dirname = base_directory + '/' + str(i_image).zfill(digits_images)
-    last_iteration = num_iterations - 1
-    last_image_name = image_dirname + '/' + str(last_iteration).zfill(digits_iterations) + '.coor'
-    dest_name = final_path_directory + '/' + str(i_image).zfill(digits_images) + '.coor'
-    print(f'Copying file from {last_image_name} to {dest_name}')
-    copyfile(last_image_name, dest_name)
+for j_copy in range(0, num_swarms):
+    dest_dirname = final_path_directory + '/copy_' + str(j_copy).zfill(digits_copies)
+    if not os.path.exists(dest_dirname):
+        os.makedirs(dest_dirname)
+    for i_image in range(0, num_images):
+        image_dirname = base_directory + '/image_' + str(i_image).zfill(digits_images)
+        copy_dirname = image_dirname + '/copy_' + str(j_copy).zfill(digits_copies)
+        last_iteration = num_iterations - 1
+        src_name = copy_dirname + '/' + str(last_iteration).zfill(digits_iterations) + '.coor'
+        dest_name = dest_dirname + '/' + str(i_image).zfill(digits_images) + '.coor'
+        print(f'Copying file from {src_name} to {dest_name}')
+        copyfile(src_name, dest_name)
--- SMwST/draw_axis.tcl
+++ SMwST/draw_axis.tcl
@@ -1,15 +1,61 @@
 proc draw_axis {} {
-    graphics top color black
-    graphics top line {-180.0 -180.0 -180.0} {-180.0 -180.0 180.0} width 2 style solid
-    graphics top line {-180.0 -180.0 -180.0} {-180.0 180.0 -180.0} width 2 style solid
-    graphics top line {-180.0 -180.0 -180.0} {180.0 -180.0 -180.0} width 2 style solid
-    graphics top line {-180.0 -180.0 180.0} {-180.0 180.0 180.0} width 2 style solid
-    graphics top line {-180.0 -180.0 180.0} {180.0 -180.0 180.0} width 2 style solid
-    graphics top line {-180.0 180.0 -180.0} {180.0 180.0 -180.0} width 2 style solid
-    graphics top line {-180.0 180.0 -180.0} {-180.0 180.0 180.0} width 2 style solid
-    graphics top line {180.0 -180.0 -180.0} {180.0 -180.0 180.0} width 2 style solid
-    graphics top line {180.0 -180.0 -180.0} {180.0 180.0 -180.0} width 2 style solid
-    graphics top line {180.0 180.0 180.0} {-180.0 180.0 180.0} width 2 style solid
-    graphics top line {180.0 180.0 180.0} {180.0 -180.0 180.0} width 2 style solid
-    graphics top line {180.0 180.0 180.0} {180.0 180.0 -180.0} width 2 style solid
+  graphics top color black
+  graphics top line {-180.0 -180.0 -180.0} {-180.0 -180.0 180.0} width 4 style solid
+  graphics top line {-180.0 -180.0 -180.0} {-180.0 180.0 -180.0} width 4 style solid
+  graphics top line {-180.0 -180.0 -180.0} {180.0 -180.0 -180.0} width 4 style solid
+  graphics top line {-180.0 -180.0 180.0} {-180.0 180.0 180.0} width 4 style solid
+  graphics top line {-180.0 -180.0 180.0} {180.0 -180.0 180.0} width 4 style solid
+  graphics top line {-180.0 180.0 -180.0} {180.0 180.0 -180.0} width 4 style solid
+  graphics top line {-180.0 180.0 -180.0} {-180.0 180.0 180.0} width 4 style solid
+  graphics top line {180.0 -180.0 -180.0} {180.0 -180.0 180.0} width 4 style solid
+  graphics top line {180.0 -180.0 -180.0} {180.0 180.0 -180.0} width 4 style solid
+  graphics top line {180.0 180.0 180.0} {-180.0 180.0 180.0} width 4 style solid
+  graphics top line {180.0 180.0 180.0} {180.0 -180.0 180.0} width 4 style solid
+  graphics top line {180.0 180.0 180.0} {180.0 180.0 -180.0} width 4 style solid
+}
+
+proc draw_ticks {} {
+  graphics top color black
+  # phi_1 ticks
+  set firsttime 1
+  for {set i -180.0} {$i <= 180.0} {set i [expr $i + 60.0]} {
+    graphics top line "$i -180.0 165.0" "$i -180.0 180.0" width 4 style solid
+    if {$firsttime < 1} {
+      set minor_tick_interval [expr 60.0 / 5.0]
+      for {set j [expr $major_tick_previous + $minor_tick_interval]} {$j < $i} {set j [expr $j + $minor_tick_interval]} {
+        graphics top line "$j -180.0 172.5" "$j -180.0 180.0" width 2 style solid
+      }
+    } else {
+      set firsttime 0
+    }
+    set major_tick_previous $i
+  }
+  # phi_2 ticks
+  set firsttime 1
+  for {set i -180.0} {$i <= 180.0} {set i [expr $i + 60.0]} {
+    graphics top line "165.0 -180.0 $i" "180.0 -180.0 $i" width 4 style solid
+    if {$firsttime < 1} {
+      set minor_tick_interval [expr 60.0 / 5.0]
+      for {set j [expr $major_tick_previous + $minor_tick_interval]} {$j < $i} {set j [expr $j + $minor_tick_interval]} {
+        graphics top line "172.5 -180.0 $j" "180.0 -180.0 $j" width 2 style solid
+      }
+    } else {
+      set firsttime 0
+    }
+    set major_tick_previous $i
+  }
+  # phi_3 ticks
+  set firsttime 1
+  for {set i -180.0} {$i <= 180.0} {set i [expr $i + 60.0]} {
+    graphics top line "-180.0 $i 180.0" "-165.0 $i 180.0" width 4 style solid
+    if {$firsttime < 1} {
+      set minor_tick_interval [expr 60.0 / 5.0]
+      for {set j [expr $major_tick_previous + $minor_tick_interval]} {$j < $i} {set j [expr $j + $minor_tick_interval]} {
+        graphics top line "-180.0 $j 180.0" "-172.5 $j 180.0" width 2 style solid
+      }
+    } else {
+      set firsttime 0
+    }
+    set major_tick_previous $i
+  }
 }
