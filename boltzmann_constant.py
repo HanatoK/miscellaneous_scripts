1ac8ab107e5189bebc31a16a515d22e61093d3dc
HanatoK <summersnow9403@gmail.com>
2022-03-04 10:35:20 -0600

    Add standard concentration

--- boltzmann_constant.py
+++ boltzmann_constant.py
@@ -14,6 +14,10 @@ boltzmann_constant_kjmolk = boltzmann_constant_jk * avogadro_constant * 1e-3
 # boltzmann in kcal/(mol*K)
 boltzmann_constant_kcalmolk = boltzmann_constant_kjmolk / cal_to_joule
 
+# standard concentration in angstrom^3
+standard_concentration = 1.0/(avogadro_constant/1e27)
+
 if __name__ == '__main__':
     print(f'{"Boltzmann constant in kJ/(mol⋅K):":36s} {boltzmann_constant_kjmolk:20.18f} ({boltzmann_constant_kjmolk:20.15e})')
     print(f'{"Boltzmann constant in kcal/(mol⋅K):":36s} {boltzmann_constant_kcalmolk:20.18f} ({boltzmann_constant_kcalmolk:20.15e})')
+    print(f'Standard concentration 1 M in Å^3: 1/{standard_concentration:20.18f}')
