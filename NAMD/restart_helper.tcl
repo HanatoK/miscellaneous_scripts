019bb6848ba4941a728c676351ab1a80b8046d43
HanatoK <summersnow9403@gmail.com>
2022-03-16 04:26:48 -0500

    NAMD script for reading restart step

---
+++ NAMD/restart_helper.tcl
@@ -0,0 +1,38 @@
+# read NAMD restart step from xsc file
+proc read_restart_step {xsc_filename} {
+  set fp [open ${xsc_filename} "r"]
+  set data [split [read $fp] "\n"]
+  set step -1
+  foreach line $data {
+    set s [string trim $line]
+    if {[string length $s] > 0} {
+      # extract the first character to see if this is a comment
+      set first_char [string index $s 0]
+      if {[string compare $first_char "#"] == 0} {
+        continue
+      } else {
+        set split_list [regexp -all -inline {\S+} $s]
+        set step [lindex $split_list 0]
+      }
+    }
+  }
+  return $step
+}
+
+proc new_restart_prefix {prefix {start_index 0}} {
+  # construct the output filename
+  set new_prefix $prefix
+  if {${start_index} > 0} {
+    set new_prefix "${prefix}+${start_index}"
+  }
+  set test_filename "${new_prefix}.restart.coor"
+  # test file existence
+  if {[file exists ${test_filename}] == 1} {
+    set new_index [expr ${start_index} + 1]
+    # find next index if not exists
+    puts "File ${test_filename} exists, test next index ${new_index}"
+    return [new_restart_prefix $prefix $new_index]
+  } else {
+    return ${new_prefix}
+  }
+}
