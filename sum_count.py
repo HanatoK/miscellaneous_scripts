59a720fe2162dfe3f4f7a3514261b59d188aea42
HanatoK <summersnow9403@gmail.com>
2022-04-28 09:04:44 -0500

    Update sum_count.py

--- sum_count.py
+++ sum_count.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python3
-import numpy as np
-import os
+# import numpy as np
+# import os
 import argparse
 
-# parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('count', nargs = '+', help = 'specify the count file(s)')
-parser.add_argument('-t', '--timestep', help = 'timestep (fs) for compute the estimated simulation time')
-args = parser.parse_args()
+def sum_count(count_file):
+    total_count = 0
+    max_count = 0
+    first_time = True
+    with open(count_file, 'r') as f_input:
+        for line in f_input:
+            if line.startswith('#'):
+                continue
+            fields = line.split()
+            if fields:
+                if first_time:
+                    print(f'The count file {count_file} has {len(fields)} data columns.')
+                    first_time = False
+                c = float(fields[-1])
+                total_count += c
+                if c > max_count:
+                    max_count = c
+    return total_count, max_count
 
-def sum_count(count_file, timestep = 2.0):
-    data = np.genfromtxt(count_file, unpack = True)
-    total_count = np.sum(data[-1])
-    estimated_time = timestep * total_count / 1e6
-    print(f'Total count of {count_file} is {total_count:.2f} ; Estimated simulation time : {estimated_time:.2f} (ns)')
-    return total_count
-
-for infile in args.count:
-    if args.timestep:
-        sum_count(infile, timestep = float(args.timestep))
-    else:
-        sum_count(infile)
+if __name__ == '__main__':
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('count', nargs = '+', help = 'specify the count file(s)')
+    parser.add_argument('-t', '--timestep', type=float, default=2.0,
+                        help='timestep (fs) for compute the estimated simulation time')
+    args = parser.parse_args()
+    for infile in args.count:
+        if args.timestep:
+            total_count, max_count = sum_count(infile)
+            estimated_time = args.timestep * total_count / 1e6
+            print(f'Total count of {infile} is {total_count:.2f}\n'
+                  f'Estimated simulation time : {estimated_time:.2f} (ns)\n'
+                  f'Max count = {max_count}')
+        else:
+            total_count, max_count = sum_count(infile)
+            print(f'Total count of {infile} is {total_count:.2f}\n'
+                  f'Max count = {max_count}')
