8d64c26101427d3717b2ec9704bed020391164ec
HanatoK <summersnow9403@gmail.com>
2023-03-13 09:49:01 -0500

    Make the collect-configuration.py more robust

--- SMwST/collect-configuration.py
+++ SMwST/collect-configuration.py
@@ -6,21 +6,21 @@ from shutil import copyfile
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('n', help = 'number of images')
+parser.add_argument('n', help='number of images')
 args = parser.parse_args()
 num_images = int(args.n)
 
 # get the number of trajectories per swarm
 output_basedir = 'output'
 output_dirs = glob(f'{output_basedir}/*')
-num_swarms = int(len(output_dirs) / num_images)
+num_swarms = len(output_dirs) // num_images
 print(f'Number of images : {num_images}')
 print(f'Number of trajectories per image : {num_swarms}')
 output_dirs.sort()
-result_dirs = output_dirs[::num_swarms]
+result_dirs = [output_dirs[(start*num_swarms):(start*num_swarms+num_swarms)] for start in range(num_images)]
 
 # get the number of iterations
-num_iterations = len(glob(result_dirs[0] + '/*.image*.iter*.coor'))
+num_iterations = len(glob(result_dirs[0][0] + '/*.image*.iter*.coor'))
 digits_iterations = len(str(num_iterations-1))
 print(f'Number of iterations: {num_iterations}')
 
@@ -30,7 +30,6 @@ if not os.path.exists(base_directory):
     os.makedirs(base_directory)
 digits_images = len(str(num_images))
 digits_copies = len(str(num_swarms))
-digits_output = len(str(num_swarms*num_images))
 for i_image in range(0, num_images):
     image_dirname = base_directory + '/image_' + str(i_image).zfill(digits_images)
     if not os.path.exists(image_dirname):
@@ -39,7 +38,7 @@ for i_image in range(0, num_images):
         copy_dirname = image_dirname + '/copy_' + str(j_copy).zfill(digits_copies)
         if not os.path.exists(copy_dirname):
             os.makedirs(copy_dirname)
-        result_dirname = output_basedir + '/' + str(i_image * num_swarms + j_copy).zfill(digits_output)
+        result_dirname = result_dirs[i_image][j_copy]
         iteration_images = glob(result_dirname + '/*.image*.iter*.coor')
         iteration_images.sort()
         for i_iteration, image_name in enumerate(iteration_images):
