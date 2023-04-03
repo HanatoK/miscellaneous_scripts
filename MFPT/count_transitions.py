b3f75b90dac2dc035e7e21de3f706b1839575be6
HanatoK <summersnow9403@gmail.com>
2023-04-03 10:30:44 -0500

    Add stddev for rates

--- MFPT/count_transitions.py
+++ MFPT/count_transitions.py
@@ -45,31 +45,40 @@ def main():
     # transition_A_to_B = [m.span() for m in re.finditer(pattern, labels_str)]
     transition_A_to_B = direct_find(labels_str, 'A', 'B')
     print(f'Number of transition_A_to_B: {len(transition_A_to_B)}')
+    time_AB = []
     with open('transition_A_to_B.csv', 'w', newline='') as f_output:
-        time_avg = 0
         fieldnames = ['step_A', 'step_B', 'label_A', 'label_B']
         writer = csv.writer(f_output)
         writer.writerow(fieldnames)
         for i, j in transition_A_to_B:
-            time_avg += data[j-1][0] - data[i][0]
+            time_AB.append(data[j-1][0] - data[i][0])
             writer.writerow([data[i][0], data[j-1][0], data[i][1], data[j-1][1]])
-        time_avg *= 0.5 / 1e3 / len(transition_A_to_B)
-        print(f'Mean first passage time from A to B is {time_avg} ps, rate k_ab is {1.0/time_avg:.3e} ps^-1')
+    time_AB = np.array(time_AB) * 0.5 / 1e3
+    print(time_AB)
+    avg_time_AB = np.average(time_AB)
+    stddev_time_AB = np.std(time_AB)
+    rate_AB = 1.0/avg_time_AB
+    stddev_rate_AB = np.sqrt(np.square(stddev_time_AB / avg_time_AB)) * rate_AB
+    print(f'Mean first passage time from A to B is {avg_time_AB} ± {stddev_time_AB} ps, rate k_ab is {rate_AB:.3e} ps^-1 ± {stddev_rate_AB:.3e} ps^-1')
     # pattern = re.compile('B[BM]*A')
     print('Start computing k_BA')
     # transition_B_to_A = [m.span() for m in re.finditer(pattern, labels_str)]
     transition_B_to_A = direct_find(labels_str, 'B', 'A')
     print(f'Number of transition_B_to_A: {len(transition_B_to_A)}')
+    time_BA = []
     with open('transition_B_to_A.csv', 'w', newline='') as f_output:
-        time_avg = 0
         fieldnames = ['step_B', 'step_A', 'label_B', 'label_A']
         writer = csv.writer(f_output)
         writer.writerow(fieldnames)
         for i, j in transition_B_to_A:
-            time_avg += data[j-1][0] - data[i][0]
+            time_BA.append(data[j-1][0] - data[i][0])
             writer.writerow([data[i][0], data[j-1][0], data[i][1], data[j-1][1]])
-        time_avg *= 0.5 / 1e3 / len(transition_B_to_A)
-        print(f'Mean first passage time from B to A is {time_avg} ps, rate k_ba is {1.0/time_avg:.3e} ps^-1')
+    time_BA = np.array(time_BA) * 0.5 / 1e3
+    avg_time_BA = np.average(time_BA)
+    stddev_time_BA = np.std(time_BA)
+    rate_BA = 1.0/avg_time_BA
+    stddev_rate_BA = np.sqrt(np.square(stddev_time_BA / avg_time_BA)) * rate_BA
+    print(f'Mean first passage time from B to A is {avg_time_BA} ± {stddev_time_BA} ps, rate k_ba is {rate_BA:.3e} ps^-1 ± {stddev_rate_BA:.3e} ps^-1')
 
 
 if __name__ == '__main__':
